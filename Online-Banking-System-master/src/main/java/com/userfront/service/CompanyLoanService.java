a789de3580095aae62821d89c1aa326b32401c4d
namvt9x <76526213+namvt9x@users.noreply.github.com>
2026-07-24 10:00:56 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "add CompanyLoanBadDebtController"""""""

--- Online-Banking-System-master/src/main/java/com/userfront/service/CompanyLoanService.java
+++ Online-Banking-System-master/src/main/java/com/userfront/service/CompanyLoanService.java
@@ -1,8 +1,5 @@
 package com.userfront.service;
 
-import java.util.List;
-
-import com.userfront.domain.CompanyLoanBadDebtReviewRequest;
 import com.userfront.domain.CompanyLoanCalculationRequest;
 import com.userfront.domain.CompanyLoanSummary;
 
@@ -11,8 +8,4 @@ public interface CompanyLoanService {
     CompanyLoanSummary getLoanSummary(Long companyId, CompanyLoanCalculationRequest request);
 
     CompanyLoanSummary getRepaymentPlan(Long companyId, CompanyLoanCalculationRequest request);
-
-    CompanyLoanSummary reviewBadDebt(Long companyId, CompanyLoanCalculationRequest request);
-
-    List<CompanyLoanSummary> reviewBadDebtList(List<CompanyLoanBadDebtReviewRequest> requests);
 }
