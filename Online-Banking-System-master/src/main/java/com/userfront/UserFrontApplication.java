3509c62afe4906d404d17043b4e4ba630901d9d2
namvt@nal.vn <namvt@nal.vn>
2026-07-29 11:15:17 +0700

    feat(payment): add

--- Online-Banking-System-master/src/main/java/com/userfront/UserFrontApplication.java
+++ Online-Banking-System-master/src/main/java/com/userfront/UserFrontApplication.java
@@ -2,11 +2,16 @@ package com.userfront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class UserFrontApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserFrontApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserFrontApplication.class, args);
+    }
 }
